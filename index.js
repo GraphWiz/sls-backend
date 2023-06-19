3c184fc9119627710ad3b8d89e15182ee8c89761
Jaypee <jignacio@cambridge.org>
2023-06-19 22:05:11 +0800

    Refactor js file

--- index.js
+++ index.js
@@ -1,58 +1,46 @@
-import express from "express";
-import serverless from "serverless-http";
+import express from 'express';
+import serverless from 'serverless-http';
 import cors from 'cors';
-import { getPrompt } from './helpers/message/index.js'
+import { getPrompt } from './helpers/message/index.js';
 import { ChatGPTAPI } from 'chatgpt';
 import bodyParser from 'body-parser';
-const { json } = bodyParser;
 
 const app = express();
+const { json, urlencoded } = bodyParser;
 
-app.use(cors({
-  methods: 'POST'
-}));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ methods: 'POST' }));
+app.use(urlencoded({ extended: true }));
 app.use(json({ limit: '5mb' }));
-
-app.use(express.json());
 
 const gpt = new ChatGPTAPI({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
+app.get('/', (_, res) => {
+  res.status(200).json({ message: 'Hello from root!' });
 });
 
-app.get("/path", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
+app.get('/path', (_, res) => {
+  res.status(200).json({ message: 'Hello from path!' });
 });
 
 app.post('/chat', async (req, res) => {
   try {
     const { type, message } = req.body;
-    const prompt = getPrompt(type, message)
-    console.log('Prompt:', prompt);
+    const prompt = getPrompt(type, message);
     const response = await gpt.sendMessage(message, {
       systemMessage: prompt
     });
     res.json({ response });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("EVENT\n" + JSON.stringify(error, null, 2));
+    res.status(500).json({ error: 'Function Error: See Cloudwatch Logs' });
   }
 });
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
+app.use((_, res) => {
+  res.status(404).json({ error: 'Not Found' });
 });
 
 const handler = serverless(app);
 export { handler };
-
